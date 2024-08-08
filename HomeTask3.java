b31eae786406c439fa2b77f5227f6a79ac1d8a2f
Muhammadcoder2001 <abduqahhorovmuhammadqobil@gmail.com>
2024-08-08 10:05:27 +0500

    HomeTask3

---
+++ HomeTask3.java
@@ -0,0 +1,31 @@
+package LinkedListHomework;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class HomeTask3 {
+
+    public  void removeElementByIndex(List list, int index){
+            if (0 <= index && index < list.size()){
+                list.remove(index);
+                System.out.println(list);
+            }else {
+                System.out.println("Invalid index " + index);
+            }
+
+    }
+
+    public static void main(String[] args) {
+
+        HomeTask3 home = new HomeTask3();
+        LinkedList <String> colors = new LinkedList<>();
+
+        colors.add("White");
+        colors.add("Orange");
+        colors.add("Black");
+        colors.add("Brown");
+        colors.add("Green");
+        home.removeElementByIndex(colors, 1);
+
+    }
+}
